0efe8067f4e88c874c26ac7e3084f368f10adb0b
Angus Sun <angussun.mail@gmail.com>
2026-01-19 14:18:15 -0500

    feat: added automatic .env access support

--- apps/api/src/main/java/com/didicook/api/ApiApplication.java
+++ apps/api/src/main/java/com/didicook/api/ApiApplication.java
@@ -3,11 +3,16 @@ package com.didicook.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
-
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
